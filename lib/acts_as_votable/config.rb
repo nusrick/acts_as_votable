06d19946cc290d1f1e8ef999f52d0aa4d6d7149b
nusrick <rick@nustechnology.com>
2015-06-10 02:26:59 +0700

    fix class name

--- lib/acts_as_votable/config.rb
+++ lib/acts_as_votable/config.rb
@@ -2,7 +2,7 @@ module ActsAsVotable
   extend self
 
   def enable_protected_attributes!
-    ActsAsVotable::Vote.class_eval do
+    ActsAsVotable::LikeItem.class_eval do
       attr_accessible :votable, :voter, :vote_scope
     end
   end
